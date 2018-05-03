bc8f153d91bb5abe8a548dbd9ecddc25e84cc38e
luca.semprini10 <luca.semprini10@studio.unibo.it>
2018-05-03 12:12:20 +0200

    Problemi con il primo esercizio...

--- src/test/scala/lecture09/PropertyTests.scala
+++ src/test/scala/lecture09/PropertyTests.scala
@@ -32,4 +32,34 @@ class TestOnLists extends Properties("Seqs") {
   property("Reverse") = forAll { (l1: Seq[Int], l2: Seq[Int]) =>
     l1.reverse.reverse == l1
   }
+
+  def associativeProp[A: Arbitrary]: Prop = forAll{(l1: Seq[A], l2: Seq[A], l3: Seq[A]) =>
+    ( l1 ++ l2 ) ++ l3 == l1 ++ ( l2 ++ l3 )
+  }
+
+  property("Associative lists") = Prop.all(
+    associativeProp[Int], associativeProp[String], associativeProp[(Boolean,Double)])
+
+  def addToEmptyProp[A: Arbitrary]: Prop = forAll{(l1: Seq[A]) =>
+     Seq.empty ++ l1 == l1
+  }
+
+  property("Add to empty list") = Prop.all(
+    addToEmptyProp[Int], addToEmptyProp[String], addToEmptyProp[(Boolean,Double)])
+
+
+  def mapIdentityProp[A: Arbitrary]: Prop = forAll{(l1: Seq[A]) =>
+    l1.map(identity) == l1
+  }
+
+  property("Map identity list") = Prop.all(
+    mapIdentityProp[Int], mapIdentityProp[String], mapIdentityProp[(Boolean,Double)])
+
+  def mapComposeProp[A: Arbitrary, B: Arbitrary, C: Arbitrary]: Prop = forAll{(f: A=> B, g: B=> C, l1: Seq[A]) =>
+    l1.map(g compose f) == l1.map(f).map(g)
+  }
+
+  property("Map compose list") = Prop.all(
+    mapComposeProp[Int, Int, Int], mapComposeProp[String, String, String], mapComposeProp[(Boolean,Double), (Boolean,Double), (Boolean,Double)])
+
 }
